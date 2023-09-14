ee86c362ba9e65c10857f443cd3483fcdad59c78
Georgios Soteriou <gesoti90@gmail.com>
2023-09-14 14:39:40 +0100

    adding linter

--- documenthelper/embeddings.py
+++ documenthelper/embeddings.py
@@ -1,8 +1,15 @@
-import typing
+"""Embeddings module.
+
+This module is responsible for adding document embeddings in our local VectorStore.
+"""
+
+import typing  # pylint: disable=unused-import
+
 
 def main() -> None:
-    print("Embeddings main!")
-    raise NotImplemented
+    """Main function is used as entrypoint for `poetry run embeddings` command."""
+    print("Adding embeddings to local VectorStore!")
+
 
 if __name__ == "__main__":
     main()
